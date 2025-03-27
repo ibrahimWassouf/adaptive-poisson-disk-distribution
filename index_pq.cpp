b128f12256792f53e6e6f42138de25e7a747bbad
Ibrahim Wassouf <ib981347@dal.ca>
2025-03-27 16:22:25 -0300

    implement indexed priority queue

---
+++ index_pq.cpp
@@ -0,0 +1,69 @@
+#include "index_pq.h"
+
+using namespace std;
+
+void IndexPQ::exchange(int i, int j, int keyi, int keyj) {
+
+  pair<double, Point> swap = pq[i];
+  pq[i] = pq[j];
+  pq[j] = swap;
+
+  // update position of element with associated key in the pq
+  qp[keyi] = j;
+  qp[keyj] = i;
+}
+
+void IndexPQ::sink(int i) {
+  while (i < pq.size()) {
+    int j = i;
+    if ((2 * i + 1) < pq.size() && pq[2 * i + 1].first > pq[j].first)
+      j = 2 * i + 1;
+    if ((2 * i + 2) < pq.size() && pq[2 * i + 2].first > pq[j].first) {
+      j += 1;
+    }
+    if (i != j) {
+      exchange(i, j, keys[pq[i].second], keys[pq[j].second]);
+      i = j;
+    } else
+      break;
+  }
+}
+
+void IndexPQ::swim(int i) {
+  while (i > 0) {
+    int j = (i - 1) / 2;
+    if (pq[i] > pq[j]) {
+      exchange(i, j, keys[pq[i].second], keys[pq[j].second]);
+      i = j;
+    } else
+      break;
+  }
+}
+
+void IndexPQ::push(pair<double, Point> el) {
+  pq.push_back(el);
+  keys[el.second] = counter;
+  qp.push_back(pq.size() - 1);
+  counter += 1;
+  swim(pq.size() - 1);
+}
+
+pair<double, Point> IndexPQ::pop() {
+  pair<double, Point> top = pq[0];
+  pair<int, int> p1 = top.second;
+  pair<int, int> p2 = pq[pq.size() - 1].second;
+  exchange(0, pq.size() - 1, keys[p1], keys[p2]);
+  // item with this key no longer in pq
+  qp[keys[p1]] = -1;
+  pq.pop_back();
+  sink(0);
+
+  return top;
+}
+
+void IndexPQ::change(int key, double w) {
+  int pq_index = qp[key];
+  pq[pq_index].first = w;
+  swim(pq_index);
+  sink(pq_index);
+}
---
+++ index_pq.h
@@ -0,0 +1,25 @@
+#ifndef INDEX_PQ_H
+#define INDEX_PQ_H
+
+#include <map>
+#include <utility>
+#include <vector>
+
+typedef std::pair<int, int> Point;
+
+class IndexPQ {
+public:
+  int counter = 0;
+  std::map<Point, int> keys;
+  std::vector<std::pair<double, Point>> pq;
+  std::vector<int> qp;
+
+  void exchange(int, int, int, int);
+  void sink(int);
+  void swim(int);
+  void push(std::pair<double, Point>);
+  std::pair<double, Point> pop();
+  void change(int, double);
+};
+
+#endif
